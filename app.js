bac137e9b5b7a46e8c220a8b342ad75a050eeca6
Iminguet <i.minguet@hotmail.com>
2023-01-08 11:08:43 +0100

    Uso de prevent default para evitarvrecargar la página al enviar un formulario

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    submitForm(event) {
+      event.preventDefault();
+      alert('Submitted');
+    },
     setName(event, lastName) {
       this.name = `${event.target.value} ${lastName}`;
     },
