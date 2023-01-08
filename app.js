41b53eedaf1957b2027520fe9c5c180859ae9734
Iminguet <i.minguet@hotmail.com>
2023-01-08 10:05:03 +0100

    entrada de datos. Comienzo

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: 'Finish the course and learn Vue ',
-      courseGoalB: 'Trabajar de esto y demostrar lo que valgo',
-      vueLink: 'https://vuejs.org/',
-      persona: {
-        nombre: 'Aureliano',
-        apellido: 'Buendía',
-        ciudad: 'Macondo',
-      },
+      counter: 0,
     };
-  },
-
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return `Nombre: ${this.persona['nombre']}, <br/> Apellido: ${this.persona.apellido}`;
-      } else {
-        return `Nombre: ${this.persona['nombre']}, <br/> Apellido: ${this.persona.apellido}`;
-      }
-    },
   },
 });
 
-app.mount('#user-goal');
+app.mount('#events');
