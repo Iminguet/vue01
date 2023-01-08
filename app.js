c825c24ed8b0d882d9e3e9ba882d81b6ca151bfe
Iminguet <i.minguet@hotmail.com>
2023-01-08 10:46:54 +0100

    objeto event desde un input. Versión sencilla.NO PARAMETROS

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
       num: 5,
     };
   },
   methods: {
-    add(num) {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add() {
       if (this.counter < 20) {
         this.counter += this.num;
       }
     },
-    remove(num) {
+    remove() {
       if (this.counter > 0) {
         this.counter -= this.num;
       }
