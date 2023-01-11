ac83302b35bf095d225deef05560ccd571bd9887
Iminguet <i.minguet@hotmail.com>
2023-01-11 10:10:34 +0100

    intro a computed

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ const app = Vue.createApp({
       name: '',
     };
   },
+
+  // los métodos computed se interpretan como propiedades, no como funciones.
+  // Es esencial no llamarlos desde el html con los parentesis ()
+  computed: {
+    fullName() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + '' + 'Buendía';
+    },
+  },
+
   methods: {
-    setName(event, lastName) {
+    setName(event) {
       this.name = event.target.value;
     },
     add(num) {
