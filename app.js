78f88c28fbb545228af1a6f073f64006ef3cebe9
Iminguet <i.minguet@hotmail.com>
2023-01-11 10:28:08 +0100

    WATCHERS. En este punto se muestra la ventaja de computed sobre watchers

--- app.js
+++ app.js
@@ -3,17 +3,37 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
+      lastName: '',
+      // fullName: '',
+      random: 0,
     };
+  },
+  watch: {
+    // name(value) {
+    //   this.random = Math.random();
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // },
   },
 
   // los métodos computed se interpretan como propiedades, no como funciones.
   // Es esencial no llamarlos desde el html con los parentesis ()
   computed: {
     fullName() {
-      if (this.name === '') {
+      if (this.name === '' || this.lastName === '') {
         return '';
       }
-      return this.name + '' + 'Buendía';
+      return this.name + ' ' + this.lastName;
     },
   },
 
@@ -30,6 +50,7 @@ const app = Vue.createApp({
     },
     resetInput() {
       this.name = '';
+      this.lastName = '';
     },
   },
 });
