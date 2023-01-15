33a4524be545ed841bb3915aa385dec926ff8933
Iminguet <i.minguet@hotmail.com>
2023-01-15 18:50:19 +0100

    componentes y multiples apps

--- app.js
+++ app.js
@@ -1,83 +1,59 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0123456789911',
+          email: 'manuel@localhost.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '09876 543 221',
+          email: 'julie@localhost.com',
+        },
+        {
+          id: 'aureliano',
+          name: 'Aureliano Buendía',
+          phone: '666 555 444 888',
+          email: 'a.buendia@localhost.com',
+        },
+      ],
+    };
+  },
+});
+
+app.component('friend-contact', {
+  template: `     
+    <li>
+          <h2>{{friend.name}}</h2>
+          <button @click="toggleDetails()">
+            {{detailsAreVisible ? 'Hide': 'Show'}} Details
+          </button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{friend.phone}}</li>
+            <li><strong>Email:</strong>{{friend.email}}</li>
+          </ul>
+        </li>`,
+
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '0123456789911',
+        email: 'manuel@localhost.com',
+      },
     };
   },
   methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
     },
-    setText() {
-      // this.message = this.currentUserInput;
-      this.message = this.$refs.userText.value;
-      // console.dir(this.$refs.userText);
-    },
-  },
-  beforeCreate() {
-    console.log('beforeCreate()');
-  },
-  created() {
-    console.log('created()');
-  },
-  beforeMount() {
-    console.log('beforeMount()');
-  },
-  mounted() {
-    console.log('mounted()');
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate()');
-  },
-  updated() {
-    console.log('updated()');
-  },
-  beforeUnmount() {
-    console.log('beforeUnmount()');
-  },
-  unmounted() {
-    console.log('unmounted()');
   },
 });
 
 app.mount('#app');
-
-setTimeout(function () {
-  app.unmount();
-}, 3000);
-
-const app2 = Vue.createApp({
-  template: `
-    <p>{{ favoriteMeal }}</p>
-  `,
-  data() {
-    return {
-      favoriteMeal: 'Pizza',
-    };
-  },
-});
-
-app2.mount('#app2');
-
-// ....
-
-const data = {
-  message: 'Hello!',
-  longMessage: 'Hello! World!',
-};
-
-const handler = {
-  set(target, key, value) {
-    if (key === 'message') {
-      target.longMessage = value + ' World!';
-    }
-    target.message = value;
-  },
-};
-
-const proxy = new Proxy(data, handler);
-
-proxy.message = 'Hello!!!!';
-
-// console.log(proxy.longMessage);
