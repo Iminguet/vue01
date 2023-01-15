e0260aefd425c77b1d7fa91ca54907b449406a25
Iminguet <i.minguet@hotmail.com>
2023-01-15 14:22:58 +0100

    Añadida llamada a rendición. v-if='(not)winner' cambiado por un sencillo v-else

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ const app = Vue.createApp({
   computed: {
     monsterBarStyles() {
       if (this.monsterHealth < 0) {
-        return { width: '0%' };
         this.monsterHealth = 0;
+        return { width: '0%' };
       }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
       if (this.playerHealth < 0) {
+        this.playerHealth = 0;
         return { width: '0%' };
       }
       return { width: this.playerHealth + '%' };
@@ -105,6 +106,10 @@ const app = Vue.createApp({
         (this.winner = null),
         (this.buttonStatusDuringGame = true);
       this.restartButtonStatus = null;
+    },
+    surrender() {
+      this.winner = 'monster';
+      this.restartButtonStatus = true;
     },
   },
 });
